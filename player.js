97724621212e216da921ac9012544a518ec4c26d
ssslowly <117667441+ssslowly@users.noreply.github.com>
2022-11-30 23:30:44 +0600

    Add files via upload

--- player.js
+++ player.js
@@ -1,24 +1,24 @@
-const player = document.querySelector('.player');
-const playBtn = document.querySelector('.play');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-const audio = document.querySelector('.audio');
-const progressContainer = document.querySelector('.progress_container');
-const progress = document.querySelector('.progress');
-const title = document.querySelector('.song');
-const cover = document.querySelector('.cover_img');
-const imgSrc = document.querySelector('.img_src');
+const player = document.querySelector('.player'),
+playBtn = document.querySelector('.play'),
+prevBtn = document.querySelector('.prev'),
+nextBtn = document.querySelector('.next'),
+audio = document.querySelector('.audio'),
+progressContainer = document.querySelector('.progress_container'),
+progress = document.querySelector('.progress'),
+title = document.querySelector('.song'),
+cover = document.querySelector('.cover_img'),
+imgSrc = document.querySelector('.img_src')
 
 // названия песен
 const songs = ['plenka - Call Me (Slowed)', 'Mareux - The Perfect Girl']
 
 // песня по умолчанию
-let songIndex = 1;
+let songIndex = 0
 
 // Init 
-function loadSong() {
+function loadSong(song) {
    title.innerHTML = song
-   audio.src = 'music/${song}.mp3'
-   cover.src = 'img/cover${songIndex + 1}.png'
+   audio.src = `music/${song}.mp3`
+   cover.src = `img/cover${songIndex + 1}.png`
 }
 loadSong(songs[songIndex])
