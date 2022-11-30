9866804cd476dc41fec5a481994c4b1d366b1be7
ssslowly <117667441+ssslowly@users.noreply.github.com>
2022-12-01 04:10:21 +0600

    Add files via upload

--- player.js
+++ player.js
@@ -7,13 +7,13 @@ progressContainer = document.querySelector('.progress_container'),
 progress = document.querySelector('.progress'),
 title = document.querySelector('.song'),
 cover = document.querySelector('.cover_img'),
-imgSrc = document.querySelector('.img_src')
+imgSrc = document.querySelector('.img_src');
 
 // названия песен
 const songs = ['plenka - Call Me (Slowed)', 'Mareux - The Perfect Girl']
 
 // песня по умолчанию
-let songIndex = 0
+let songIndex = 1
 
 // Init 
 function loadSong(song) {
@@ -22,3 +22,54 @@ function loadSong(song) {
    cover.src = `img/cover${songIndex + 1}.png`
 }
 loadSong(songs[songIndex])
+
+// play 
+function playSong(){
+   player.classList.add('play')
+   cover.classList.add('active')
+   imgSrc.src = 'img/pause-button.png'
+   audio.play()
+}
+
+// pause 
+function pauseSong(){
+   player.classList.remove('play')
+   cover.classList.remove('active')
+   imgSrc.src = 'img/play-button.png'
+   audio.pause()
+}
+playBtn.addEventListener('click', () => {
+   const isPlaying = player.classList.contains('play')
+   if(isPlaying){
+      pauseSong()
+   }
+   else{
+      playSong()
+   }
+})
+
+// next song
+function nextSong() {
+   songIndex++
+
+   if(songIndex > songs.length -1) {
+      songIndex = 0
+   }
+
+   loadSong(songs[songIndex])
+   playSong()
+}
+nextBtn.addEventListener('click', nextSong)
+
+// prev song
+function prevSong() {
+   songIndex--
+
+   if(songIndex < 0) {
+      songIndex = songs.length -1
+   }
+   loadSong(songs[songIndex])
+   playSong()
+
+}
+prevBtn.addEventListener('click', prevSong)
